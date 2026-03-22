0acac8348dafe32004c5bff4ae0be5d147f7a366
Bruno-nog <bqueiroz.nog@gmail.com>
2026-03-22 19:39:41 -0300

    added m2 ex03

--- m2/ex03/fewshot.py
+++ m2/ex03/fewshot.py
@@ -1,7 +1,36 @@
 import sys
+import os
+from groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+
+def generate_response(prompt):
+    examples = """
+    Traduza termos técnicos de informática.
+    Responda apenas com a tradução, sem explicações.
+
+    SQL → linguagem de consulta estruturada
+    HTML → linguagem de marcação de hipertexto
+    CPU → unidade central de processamento
+    CSS → folhas de estilo em cascata
+    """
+    prompt_final = examples + "\n" + prompt + " →"
+    client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+    response = client.chat.completions.create(
+    messages = [
+        {
+            "role": "user",
+            "content": prompt_final,
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+    )
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Needs an argument")
+        print("Usage: python3 fewshot.py <prompt>")
         sys.exit(1)
+    prompt = sys.argv[1]
+    generate_response(prompt)
