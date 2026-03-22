7ce913a4246d5419a14fc004761d3279a17d9610
Bruno-nog <bqueiroz.nog@gmail.com>
2026-03-22 18:02:30 -0300

    m2 ex02 increased and starting ex03

--- m2/ex02/prompt.py
+++ m2/ex02/prompt.py
@@ -1,10 +1,12 @@
 import os
 import sys
 from groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_api_ai(prompt, temperature):
     client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
-    mangoloko = client.chat.completions.create(
+    response = client.chat.completions.create(
     messages=[
         {
             "role": "user",
@@ -14,16 +16,21 @@ def get_api_ai(prompt, temperature):
     model="llama-3.3-70b-versatile",
     temperature=temperature,
     )
-    print(mangoloko.choices[0].message.content)
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 3 or len(sys.argv) < 2:
-        print("Needs a prompt")
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("Usage: python3 prompt.py <prompt> [temperature]")
         sys.exit(1)
-    # float(temperature) == 0.5
+    temperature = 0.5
     prompt = sys.argv[1]
     if len(sys.argv) == 3:
-        temperature = float(sys.argv[2])
-    print(temperature)
+        try:
+            temperature = float(sys.argv[2])
+        except:
+            print("temperature must be a number")
+            sys.exit(1)
+        if temperature > 2.0 or temperature < 0.0:
+            print("temperature must be between 0.0 and 2.0")
+            sys.exit(1)
     get_api_ai(prompt, temperature)
-    print(temperature)
