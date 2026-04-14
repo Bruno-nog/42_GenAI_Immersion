0917500b7579621faa4fa328a43accd48c14b502
Bruno-nog <bqueiroz.nog@gmail.com>
2026-04-14 19:07:32 -0300

    file loaded

--- m3/ex04/rag.py
+++ m3/ex04/rag.py
@@ -5,10 +5,18 @@ from groq import Groq
 from dotenv import load_dotenv
 load_dotenv()
 
+def rag(question):
+    with open("orbit_motordrones.txt", 'r') as file:
+        lines = file.readlines()
+        i = 1
+        for line in lines:
+            print(f"index {i}: {line}")
+            i += 1
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 rag.py <orbit family>")
         sys.exit(1)
-    rag = sys.argv[1]
+    question = sys.argv[1]
+    rag(question)
